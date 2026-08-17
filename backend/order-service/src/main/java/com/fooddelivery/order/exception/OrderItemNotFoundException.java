c535d5703a4b9b7f7afce3643942d69b1b6711e7
LeelaSree10 <Leela.Sree@bmwtechworks.in>
2026-08-17 11:49:33 +0530

    Revert "Complete order service cart and order APIs"

--- backend/order-service/src/main/java/com/fooddelivery/order/exception/OrderItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.fooddelivery.order.exception;
-
-public class OrderItemNotFoundException extends RuntimeException {
-
-    public OrderItemNotFoundException(Long itemId) {
-        super("Order item not found with id: " + itemId);
-    }
-}
